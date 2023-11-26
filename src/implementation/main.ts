e60296ca0980187ac651d9a5f778ddf10e242d03
Pietro Piva Vieira <pietro.developer@gmail.com>
2023-11-26 14:53:49 -0300

    feat: add first test results

--- src/implementation/main.ts
+++ src/implementation/main.ts
@@ -1,13 +1,16 @@
+import { createWriteStream } from 'node:fs';
+
 import { Item } from "./entities/item";
 import { generateRandomItems } from "./helpers/generate-random-items";
 import { bruteForce, memoized } from "./solution";
 
-const [testsRaw, knapsackMaxWeightRaw, numberOfItemsRaw, stepRaw] = process.argv.slice(2);
+const [testsRaw, knapsackMaxWeightRaw, numberOfItemsRaw, stepRaw, shouldOutputTestResultsRaw] = process.argv.slice(2);
 
 const tests = Number(testsRaw);
 let knapsackMaxWeight = Number(knapsackMaxWeightRaw);
 let numberOfItems = Number(numberOfItemsRaw);
 const step = Number(stepRaw);
+const shouldOutputTestResults = Boolean(shouldOutputTestResultsRaw);
 
 console.log('Knapsack Problem');
 
@@ -18,6 +21,13 @@ const measurements = {
 console.log();
 
 const testSet: Item[][] = [];
+
+type TestResult = Record<string, Record<number, Record<string, number>>>;
+
+const testsResults: TestResult = {
+  bruteForce: {},
+  memoized: {},
+};
 
 console.log('Generating Test Set...');
 for (let i = 0; i < tests; i++) {
@@ -53,6 +63,15 @@ for (let i = 0; i < tests; i++) {
   console.log(`Test ${i + 1} iterations: ${measurements.iterations}`);
   console.log()
 
+  if (shouldOutputTestResults) {
+    testsResults.bruteForce[i + 1] = {
+      knapsackMaxWeight,
+      inputSize: testSet[i].length,
+      executionTimeInMS: recDifference,
+      iterations: measurements.iterations,
+    };
+  }
+
   measurements.iterations = 0;
 }
 
@@ -80,6 +99,47 @@ for (let i = 0; i < tests; i++) {
   console.log(`Test ${i + 1} iterations: ${measurements.iterations}`)
   console.log();
 
+  if (shouldOutputTestResults) {
+    testsResults.memoized[i + 1] = {
+      knapsackMaxWeight,
+      inputSize: testSet[i].length,
+      executionTimeInMS: dynDifference,
+      iterations: measurements.iterations,
+    };
+  }
+
   knapsackMaxWeight += step;
   measurements.iterations = 0;
+}
+
+if (shouldOutputTestResults) {
+  console.log('Processing test results to CSV file...');
+
+  const csvHeader = `implementation,test,knapSackMaxWeight,inputSize,executionTimeInMS,iterations\n`;
+
+  const bruteForceResults = Object.entries(testsResults.bruteForce);
+  const bruteForceLines = bruteForceResults.map(([key, value]) => {
+    return `bruteForce,${key},${value.knapsackMaxWeight},${value.inputSize},${value.executionTimeInMS},${value.iterations}\n`
+  });
+
+  const memoizedResults = Object.entries(testsResults.memoized);
+  const memoizedLines = memoizedResults.map(([key, value]) => {
+    return `memoized,${key},${value.knapsackMaxWeight},${value.inputSize},${value.executionTimeInMS},${value.iterations}\n`;
+  });;
+
+  const resultFileWriteStream = createWriteStream('./results.csv', {
+    encoding: 'utf-8',
+  });
+
+  resultFileWriteStream.write(csvHeader, () => {
+    console.log('Writed CSV header...');
+  });
+
+  bruteForceLines.forEach((line) => resultFileWriteStream.write(line, () => {
+    console.log(`Wrote ${line}`);
+  }));
+
+  memoizedLines.forEach((line) => resultFileWriteStream.write(line, () => {
+    console.log(`Wrote ${line}`);
+  }));
 }
--- test.sh
+++ test.sh
@@ -8,13 +8,19 @@ TESTS=$1
 KNAPSACK_SIZE=$2
 NUMBER_OF_ITEMS=$3
 STEP=$4
+SHOULD_OUTPUT_TEST_RESULTS=$5
 
 echo 'Knapsack Algorithm Test Runner'
+
+if [ ! -d ./node_modules ]; then
+  echo "Dependencies missing, installing..."
+  npm install
+fi
 
 echo 'Compiling TypeScript source code...'
 npm run build
 
 echo 'Tests commencing:'
-npm run execute -- "$TESTS" "$KNAPSACK_SIZE" "$NUMBER_OF_ITEMS" "$STEP"
+npm run execute -- "$TESTS" "$KNAPSACK_SIZE" "$NUMBER_OF_ITEMS" "$STEP" "$SHOULD_OUTPUT_TEST_RESULTS"
 
 
